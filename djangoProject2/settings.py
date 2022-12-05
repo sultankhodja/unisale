bf027e1cf7eb03b1e13247266df5afe3c08e105a
Ubuntu <ubuntu@ip-172-31-88-199.ec2.internal>
2022-12-05 04:37:48 +0000

    requirements updated

--- djangoProject2/settings.py
+++ djangoProject2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v-h(%377nxa#w0js7)x)br*8t%h^i3jrfn+ki6p_y-r%w(((t='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
